5f99da1776f3a909d516d5eb15b3240c28f23ba0
Praveen <praveennandipati@gmail.com>
2024-11-12 08:00:37 +0530

    commit tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1 className="text-5xl font-bold color-red-500">Welcome to my First React Project with Vite </h1>
+      <h1 className="text-3xl font-bold text-red-500">Welcome to my First React Project with Vite </h1>
     </div>
   );
 }
